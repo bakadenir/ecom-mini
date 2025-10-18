ccf73ba76636a3ccc00f284664a318ddca38db04
unknown <bakadenir@gmail.com>
2025-10-18 11:29:52 +0700

    add post put delete

--- src/index.js
+++ src/index.js
@@ -14,26 +14,99 @@ app.get("/", (req, res) => {
   res.send("I dont Know")
 })
 
+// GET Product
 app.get("/products", async (req, res) => {
   const products  = await prisma.product.findMany();
 
   res.send(products);
 })
 
+// POST Product
 app.post("/products", async (req, res) => {
   const newProductData = req.body;
 
-  const products  = await prisma.product.create({
+  const product  = await prisma.product.create({
     data: {
       name: newProductData.name,
       price: newProductData.price,
       description: newProductData.description,
       image: newProductData.image,
-    }
+    },
   });
 
-  res.status(201).send("Create product success");
-})
+  res.send({
+    data: product,
+    message: "Create product success"
+  });
+});
+
+// Delete Product
+app.delete("/products/:id", async (req, res) => {
+  const productId = Number(req.params.id);
+
+  const deleteProduct = await prisma.product.delete({
+    where : {
+      id : productId,
+    },
+  });
+
+  res.send({
+    data: deleteProduct,
+    message: "Delete product success"
+  });
+});
+
+// Put
+app.put ("/products/:id", async (req, res) => {
+  const productId = Number(req.params.id);
+  const productData = req.body;
+
+  if(!(productData.name && productData.price && productData.description && productData.image)) {
+    return res.send("Fields is missing!");
+  }
+
+  const putProduct = await prisma.product.update({
+    where : {
+      id : productId,
+    },
+
+    data: {
+      name: productData.name,
+      price: productData.price,
+      description: productData.description,
+      image: productData.image,
+    },
+  });
+
+  res.send({
+    data: putProduct,
+    message: "Edit product success"
+  });
+});
+
+// PATCH
+app.patch ("/products/:id", async (req, res) => {
+  const productId = Number(req.params.id);
+  const productData = req.body;
+
+  const putProduct = await prisma.product.update({
+    where : {
+      id : productId,
+    },
+
+    data: {
+      name: productData.name,
+      price: productData.price,
+      description: productData.description,
+      image: productData.image,
+    },
+  });
+
+  res.send({
+    data: putProduct,
+    message: "Edit product success"
+  });
+});
 
 
 
